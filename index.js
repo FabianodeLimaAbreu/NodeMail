7dcc07d93b732dda36e0223a9a460b8a992b058e
Fabiano de Lima Abreu <fabianodelimaabreu@gmail.com>
2018-03-08 16:09:09 -0300

    sendEmail method

--- index.js
+++ index.js
@@ -7,6 +7,48 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.bodyParser());
 app.use(express.json());
 
+app.post('/sendEmail', function (req, res){
+    /*
+		{
+			"to":"fabianodelimaabreu@gmail.com",
+			"subject":"FocusConnect [Focus Textil] - Inscricao confirmada",
+			"template":0,
+			"obj":{
+			    "id": 1113782473,
+			    "NOME": "Ailton Paizani",
+			    "RG": "11111",
+			    "CPF": 22222,
+			    "EMAIL": "fabianoabreu@focustextil.com.br",
+			    "DDD": "11",
+			    "TEL": "34332443",
+			    "GESTOR": "Ailton"
+		  	}
+		}
+	*/
+
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+		  	user: 'SeuEmail@SeuEmail.com.br',
+	  		pass: 'SuaSenha'
+		}
+	});
+
+	var mailOptions = {
+		from: 'remetente@gmail.com',
+		to: 'destinatario@outlook.com',
+		subject: 'Sending Email using Node.js',
+		text: 'That was easy!'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+		  console.log(error);
+		} else {
+		  console.log('Email sent: ' + info.response);
+		}
+	});
+});
 
 var server = http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
